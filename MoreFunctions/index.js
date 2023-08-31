c97da2c5b8c103bed1b53ce0bffb88f8739e37e9
ccbusby123 <137566179+ccbusby123@users.noreply.github.com>
2023-08-30 22:54:10 -0500

    first commit

---
+++ MoreFunctions/index.js
@@ -0,0 +1,21 @@
+//Three Functions
+
+//Function 1 - No parameter gretting
+function greet() {
+  alert("Hello World!");
+}
+greet();
+
+
+//Function 2 - Include Name in Greeting
+function greet(name) {
+  alert("Hello, " + name + "!");
+}
+greet("Dave");
+
+
+//Function 3 - Summing two numbers
+function addNumbers(n1, n2) {
+  return n1 + n2;
+}
+alert ((4,4));
