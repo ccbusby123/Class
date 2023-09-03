0e838f9211a99bd651d3f85d7842ff79517da9fa
ccbusby123 <137566179+ccbusby123@users.noreply.github.com>
2023-09-02 19:33:07 -0500

    Add files via upload

---
+++ arrow.js
@@ -0,0 +1,14 @@
+//A function that accepts two arguments and returns their product.
+let Tot = (a, b) => a * b;
+alert(Tot(4, 2));
+
+
+//A function that takes in a string and returns it in uppercase.
+let double = n => n.toUpperCase();
+alert( double("Code Labs") );
+
+
+//A function with a single parameter, calculating the square of the provided number.
+
+let makeSquare = n => Math.pow(n,2);
+alert(makeSquare (5));
